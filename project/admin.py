4be0b412d4b671b7c7f6fe056305da64f1224182
Abdurazzoq Abdusalomov <rayyan.dev@mail.ru>
2024-01-29 16:31:12 +0500

    admin panel

--- project/admin.py
+++ project/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Genre, CarouselItem, Book
 
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = [
+        x.name for x in Book._meta.fields if x.name not in ['description']]
+    list_filter = ['shop', 'genres__name']
+
+
 admin.site.register(Genre)
 admin.site.register(CarouselItem)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
